11aeb6f7a27861bdaf92deadb7b0808675dfff8f
Andrew Caunce <andrew.caunce@airsweb.com>
2016-07-18 14:13:22 +0100

    Angular app working

--- RestService/AngularClient/Scripts/App.js
+++ RestService/AngularClient/Scripts/App.js
@@ -1,1 +1,9 @@
-﻿
+﻿var app = angular.module("RestApp", []);
+
+app.controller("SpeciesController", ["$scope", "$http", function($scope, $http) {
+    $http.get("http://swapi.co/api/species/")
+        .then(function(r) {
+            $scope.Species = r.data.results;
+        });
+
+}])
