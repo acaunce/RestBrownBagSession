95cfa4c83011268cc16ede2f7d02486bbd808ea1
Andrew Caunce <andrew.caunce@airsweb.com>
2016-07-18 14:53:40 +0100

    get the wpf version working

--- RestService/AngularClient/Scripts/App.js
+++ RestService/AngularClient/Scripts/App.js
@@ -1,9 +1,28 @@
-﻿var app = angular.module("RestApp", []);
+﻿var app = angular.module("RestApp", ["ui.bootstrap"]);
 
-app.controller("SpeciesController", ["$scope", "$http", function($scope, $http) {
+app.controller("SpeciesController", ["$scope", "$http", function ($scope, $http) {
+    var getFaves = function() {
+        $http.get("http://localhost:50421/favourites")
+                .then(function (r) {
+                    var faves = r.data;
+
+                    angular.forEach(faves, function (f) {
+                        angular.forEach($scope.Species, function (s) {
+                            if (s.name === f.Name) s.Favourite = true;
+                        });
+                    });
+                });
+    }
     $http.get("http://swapi.co/api/species/")
         .then(function(r) {
             $scope.Species = r.data.results;
+            getFaves();
         });
 
+    $scope.FavouriteSpecies = function(s) {
+        $http.post("http://localhost:50421/favourites", { Name: s.name })
+            .then(function(r) {
+                getFaves();
+            });
+    }
 }])
--- RestService/WpfClient/MainWindow.xaml.cs
+++ RestService/WpfClient/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using RestDemo.Data;
 
 namespace WpfClient
 {
@@ -20,9 +25,41 @@ namespace WpfClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<SpeciesViewModel> Species { get; } = new ObservableCollection<SpeciesViewModel>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            using (var client = new HttpClient
+            {
+                BaseAddress = new Uri("http://swapi.co/api/"),
+                DefaultRequestHeaders = { Accept = { MediaTypeWithQualityHeaderValue.Parse("application/json") } }
+            })
+            {
+                var response = client.GetAsync("species").Result;
+
+                var json = response.Content.ReadAsStringAsync().Result;
+
+                var species = JsonConvert.DeserializeObject<SwapiResult<SwapiSpecies>>(json);
+
+                foreach (var speciesViewModel in species.Results.Select(s => new SpeciesViewModel
+                {
+                    Classification = s.Classification,
+                    Designation = s.Designation,
+                    Favourite = false,
+                    Language = s.Language,
+                    Name = s.Name
+                }).ToList())
+                {
+                    Species.Add(speciesViewModel);
+                }
+            }
         }
+    }
+
+    public class SpeciesViewModel : SwapiSpecies
+    {
+        public bool Favourite { get; set; }
     }
 }
