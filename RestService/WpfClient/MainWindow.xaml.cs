a564a937732ab8b11eed9f1d84b74f1246b1d515
Andrew Caunce <andrew.caunce@airsweb.com>
2016-07-18 16:04:18 +0100

    final pass of the wpf version

--- RestService/WpfClient/MainWindow.xaml.cs
+++ RestService/WpfClient/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Mime;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,7 +26,7 @@ namespace WpfClient
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public ObservableCollection<SpeciesViewModel> Species { get; } = new ObservableCollection<SpeciesViewModel>();
 
@@ -31,6 +34,11 @@ namespace WpfClient
         {
             InitializeComponent();
 
+            GetData();
+        }
+
+        private void GetData()
+        {
             using (var client = new HttpClient
             {
                 BaseAddress = new Uri("http://swapi.co/api/"),
@@ -42,7 +50,7 @@ namespace WpfClient
                 var json = response.Content.ReadAsStringAsync().Result;
 
                 var species = JsonConvert.DeserializeObject<SwapiResult<SwapiSpecies>>(json);
-
+                Species.Clear();
                 foreach (var speciesViewModel in species.Results.Select(s => new SpeciesViewModel
                 {
                     Classification = s.Classification,
@@ -52,14 +60,112 @@ namespace WpfClient
                     Name = s.Name
                 }).ToList())
                 {
+                    speciesViewModel.FavouriteCommand = new FavouriteCommand(GetFaves);
                     Species.Add(speciesViewModel);
                 }
+                GetFaves();
             }
+        }
+        public void GetFaves()
+        {
+            using (var client = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:50421/"),
+                DefaultRequestHeaders = { Accept = { MediaTypeWithQualityHeaderValue.Parse("application/json") } }
+            })
+            {
+                var response = client.GetAsync("favourites").Result;
+
+                var json = response.Content.ReadAsStringAsync().Result;
+
+                var species = JsonConvert.DeserializeObject<FavouriteSpecies[]>(json);
+
+                foreach (var speciesViewModel in Species)
+                {
+                    speciesViewModel.Favourite = false;
+                    foreach (var fave in species)
+                    {
+                        if (speciesViewModel.Name == fave.Name) speciesViewModel.Favourite = true;
+                    }
+                }
+                OnPropertyChanged("Species");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Refresh(object sender, RoutedEventArgs e)
+        {
+            GetData();
         }
     }
 
-    public class SpeciesViewModel : SwapiSpecies
+    public class FavouriteCommand : ICommand
     {
-        public bool Favourite { get; set; }
+        private readonly Action _getFaves = null;
+
+        public FavouriteCommand(Action getFaves)
+        {
+            _getFaves = getFaves;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            using (var client = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:50421/"),
+                DefaultRequestHeaders = { Accept = { MediaTypeWithQualityHeaderValue.Parse("application/json") } }
+            })
+            {
+                var row = parameter as SpeciesViewModel;
+                var result = client.PostAsync("favourites", new StringContent(JsonConvert.SerializeObject(new { Name = row.Name }), Encoding.UTF8, "application/json")).Result;
+                row.Favourite = true;
+
+                _getFaves.Invoke();
+            }
+
+        }
+
+        public event EventHandler CanExecuteChanged;
     }
+
+
+    public class SpeciesViewModel : SwapiSpecies, INotifyPropertyChanged
+    {
+        private bool _favourite;
+        public bool Favourite
+        {
+            get { return _favourite; }
+            set
+            {
+                _favourite = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand FavouriteCommand { get; set; }
+
+        public SpeciesViewModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+
 }
