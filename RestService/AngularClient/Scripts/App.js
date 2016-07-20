be2b451020de4efdfbedbba2bcdb34182892fb30
Andrew Caunce <andrew.caunce@airsweb.com>
2016-07-20 12:41:47 +0100

    oisdhk

--- RestService/AngularClient/Scripts/App.js
+++ RestService/AngularClient/Scripts/App.js
@@ -2,6 +2,8 @@
 
 app.controller("SpeciesController", [
     "$scope", "$http", function($scope, $http) {
-
+        $http.get("https://swapi.co/api/species", function(res) {
+            $scope.Species = res.data.results;
+        });
     }
 ]);
